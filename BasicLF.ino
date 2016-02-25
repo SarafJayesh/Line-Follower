3f1f1b623089bc20b9da6c59775031d31da3cef3
SarafJayesh <jay.saraf.7@gmail.com>
2016-02-26 00:51:53 +0530

    Update BasicLF.ino

--- BasicLF.ino
+++ BasicLF.ino
@@ -4,12 +4,12 @@
  * Author: Jayesh Saraf
  */
 
-int Lsensor = 4;
-int Rsensor = 5;
-int LmotorF = 8;                            //High output on LmotorF will move the motor forward.
-int LmotorB = 9;                            //High output on LmotorB will move the motor backward.
-int RmotorF = 10;
-int RmotorB = 11;
+const int Lsensor = 5;
+const int Rsensor = 6;
+const int LmotorF = 8;                            //High output on LmotorF will move the motor forward.
+const int LmotorB = 9;                            //High output on LmotorB will move the motor backward.
+const int RmotorF = 10;
+const int RmotorB = 11;
 
 void setup()
 { PinMode(Lsensor,INPUT);
@@ -21,19 +21,32 @@ void setup()
 }
 
 void loop()
-{ if(DigitalRead(Lsensor) == HIGH)           //When left sensor is on the black line. 
-    { DigitalWrite(RmotorF,HIGH);
+{ if(DigitalRead(Lsensor) == HIGH && DigitalRead(Rsensor) == LOW)          //When left sensor is on the black line. 
+    { DigitalWrite(LmotorF,LOW);
       DigitalWrite(LmotorB,HIGH);
+      DigitalWrite(RmotorF,HIGH);
+      DigitalWrite(RmotorB,LOW);
     }
    
-  else if(DigitalRead(Rsensor) == HIGH)      //When right sensor is on the black line. 
+  else if(DigitalRead(Lsensor) == LOW && DigitalRead(Rsensor) == HIGH)      //When right sensor is on the black line. 
     { DigitalWrite(LmotorF,HIGH);
-      DigitalWrite(RmotorB,HIHG);
+      DigitalWrite(LmotorB,LOW);
+      DigitalWrite(RmotorF,LOW);
+      DigitalWrite(RmotorB,HIGH);
     }
     
-  else                                       //None of the sensor is on the black line.
-    { DigitalWrite(LmotorF,HIGH);             
+  else if(DigitalRead(Lsensor) == LOW && DigitalRead(Rsensor) == LOW)        //None of the sensors are on the black line.
+    { DigitalWrite(LmotorF,HIGH); 
+      DigitalWrite(LmotorB,LOW);
       DigitalWrite(RmotorF,HIGH);
+      DigitalWrite(RmotorB,LOW);
+    }
+  
+  else
+    { DigitalWrite(LmotorF,LOW); 
+      DigitalWrite(LmotorB,LOW);
+      DigitalWrite(RmotorF,LOW);
+      DigitalWrite(RmotorB,LOW);
     }
 }    
     
